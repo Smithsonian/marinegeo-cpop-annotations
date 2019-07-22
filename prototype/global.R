178c3aad66e040e1f65622fa988b1ef34934f1b1
Michael Lonneman <mlonneman@gmail.com>
2019-07-22 19:51:22 -0400

    import data only when selected by user

--- prototype/global.R
+++ prototype/global.R
@@ -11,74 +11,21 @@ library(dplyr)
 library(tidyr)
 library(data.table)
 
-# Sites and sensors currently available
-# This will be automatically generated in future iterations
-site_list <- c("SERC", "STRI")
-sensor_list <- c("water quality", "MET")
+# The key is an inventory of available CSVs that can be imported
+# and includes year - sensor - status (QAQC or RAW) - site information based on filepaths
+key <- as.data.frame(list(list.files("./data/")), col.names = "filepath") %>%
+  filter(filepath != "headers.csv") %>%
+  mutate(name = gsub(".csv", "", filepath),
+         # Imported tracks whether the application has imported particular datasets
+         imported = FALSE) %>%
+  # Separate file name into columns
+  separate(name, into = c("year", "sensor", "status", "site"), by = "_") %>%
+  mutate(sensor = gsub("([a-z])([A-Z])", "\\1 \\2", sensor, perl = TRUE)) %>%
+  filter(status == "QAQC")
+
+# list of parameters for available sensors
+parameters <- fread("./data/headers.csv")
 # Parameters that shouldn't be selectable by the user
 ignore_list <- c("Timestamp", "Site", "Status")
 
-# read in data
-waterquality <- fread("./data/2015_Water_Quality_QAQC_SERC.csv") %>%
-  bind_rows(fread("./data/2016_Water_Quality_QAQC_SERC.csv")) %>%
-  bind_rows(fread("./data/2017_Water_Quality_QAQC_SERC.csv")) %>%
-  bind_rows(fread("./data/2018_Water_Quality_RAW_SERC.csv")) %>%
-  bind_rows(fread("./data/2019_Water_Quality_RAW_SERC.csv")) %>%
-  bind_rows(fread("./data/2015_Water_Quality_QAQC_STRI.csv")) %>%
-  bind_rows(fread("./data/2016_Water_Quality_QAQC_STRI.csv")) %>%
-  bind_rows(fread("./data/2017_Water_Quality_QAQC_STRI.csv")) %>%
-  mutate(Timestamp = mdy_hm(Timestamp))
-
-MET <- fread("./data/2013_MET_QAQC_STRI.csv") %>%
-  bind_rows(fread("./data/2014_MET_QAQC_STRI.csv")) %>%
-  bind_rows(fread("./data/2015_MET_QAQC_STRI.csv")) %>%
-  bind_rows(fread("./data/2016_MET_QAQC_STRI.csv")) %>%
-  bind_rows(fread("./data/2017_MET_QAQC_STRI.csv")) %>%
-  bind_rows(fread("./data/2015_MET_QAQC_SERC.csv")) %>%
-  bind_rows(fread("./data/2016_MET_QAQC_SERC.csv")) %>%
-  bind_rows(fread("./data/2017_MET_QAQC_SERC.csv")) %>%
-  mutate(Timestamp = mdy_hm(Timestamp))
-
-# waterlevel <- fread("./data/2015_Water_Level_QAQC_SERC.csv") %>%
-#   bind_rows(fread("./data/2016_Water_Level_QAQC_SERC.csv")) %>%
-#   bind_rows(fread("./data/2017_Water_Level_QAQC_SERC.csv")) %>%
-#   bind_rows(fread("./data/2018_Water_Level_RAW_SERC.csv")) %>%
-#   bind_rows(fread("./data/2019_Water_Level_RAW_SERC.csv")) %>%
-#   mutate(Timestamp = mdy_hm(Timestamp))
-  
-# "pretty" labels for graph display 
-# not yet functional
-waterquality_labels <- c("Timestamp" = "Timestamp", 
-                         "Temperature" = "Temperature (°C)", 
-                         "Specific.Conductivity"="Specific Conductivity (mS/cm)",
-                         "Conductivity" = "Conductivity (mS/cm)",
-                         "TDS" = "TDS (g/L)",
-                         "Salinity"= "Salinity (psu)",
-                         "Dissolved.Oxygen"= "DO (%)",
-                         "Dissolved.Oxygen.2"= "DO (mg/l)",
-                         "Pressure"= "Pressure (psi)",
-                         "Depth"= "Depth (meters)",
-                         "pH" = "pH",
-                         "Turbidity"= "Turbidity (FNU)",
-                         "Chlorophyll"= "Chlorophyll (RFU)",
-                         "Chlorophyll.2"= "Chlorophyll (µg/l)",
-                         "BGA.PE"= "BGA-PE (RFU)",
-                         "BGA.PE 2"= "BGA-PE (µg/l)",
-                         "FDOM"= "FDOM (RFU)",
-                         "FDOM.2"= "FDOM (ppb QSE)",
-                         "Battery"= "Battery (Volts)", 
-                         "Site" = "site"
-)
-
-met.labels <- c("Air.Temperature"="Air Temperature (°C)",
-                "Barometric.Pressure"="Barometric Pressure (mbar)",
-                "Daily.Rain"="Daily Rain (mm)",
-                "PAR"="PAR",
-                "Rain.Rate"="Rain Rate (mm/hr)",
-                "Relative.Humidity"="Relative Humidity (%)",
-                "Wind.Direction"="Wind Direction (°)",
-                "Wind.Gust"="Wind Gust (mph)",
-                "Wind.Speed"="Wind Speed (mph)",
-                "Solar.Radiation" = "Solar Radiation (mm/hr)"
-)
 
--- prototype/ui.R
+++ prototype/ui.R
@@ -7,17 +7,10 @@ ui <- fluidPage(
     sidebarPanel(
       # Dropdown selector for sites
       selectInput("site", "Select one or more sites to display", 
-                  site_list, multiple = TRUE), 
+                  unique(key$site), multiple = TRUE), 
       
       # Does not currently update dynamically: All sensors will be listed if a site is selected that doesn't have all possible sensors
-      selectInput("sensor", "Select a sensor", c("none selected", sensor_list)), 
-      
-      # Select a time period to view 
-      # selectInput("timeSpan", "View data from previous:",
-      #             c("3 hours" = 3, "6 hours" = 6, "12 hours" = 12, "1 day" = 24, "2 days" = 2*24, "3 days" = 3*24,
-      #               "4 days" = 4*24, "5 days" = 5*24, "1 Week" = 7*24, "10 days" = 10*24, "2 Weeks" = 2*7*24,
-      #               "3 Weeks" = 3*7*24, "1 Month" = 31*24, "6 Weeks" = 6*7*24, "2 Months" = 31*2*24,
-      #               "year" = 24 * 365, "2 years" = 48 * 365), selected = 5*24),
+      selectInput("sensor", "Select a sensor", c("none selected", unique(key$sensor))), 
       
       # select a time range 
       dateRangeInput("date_range", "Select a time range",
@@ -25,6 +18,9 @@ ui <- fluidPage(
       
       # conditional dropdown for sensor parameters at selected site and sensor
       uiOutput('parameterSelector'),
+      
+      # button that triggers data import, if necessary, and triggers plot creation 
+      actionButton("runQuery", "Visualize data"),
       
       width = 3 # the default is 4 (of 12 total units)
     ),
